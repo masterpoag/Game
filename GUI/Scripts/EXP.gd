1cb35701a45f60d9c2af9730a0588f6438eaf948
masterpoag <72989636+masterpoag@users.noreply.github.com>
2024-01-05 16:55:57 -0600

    Fixed the GUI with the new update

--- GUI/Scripts/EXP.gd
+++ GUI/Scripts/EXP.gd
@@ -7,7 +7,6 @@ func _on_interface_exp_change(value):
 		target_exp = line[0]
 		var max_exp = line[1]
 		max_value = max_exp
-		$bar.value = target_exp
-		$bar.max_value = max_exp
-		$NinePatchRect/Label.text = "%s/%s" % [target_exp,max_exp]
-
+		$CenterContainer/HBoxContainer/bar.value = target_exp
+		$CenterContainer/HBoxContainer/bar.max_value = max_exp
+		$CenterContainer/HBoxContainer/bar/Label.text = "%s/%s" % [target_exp,max_exp]
--- GUI/Scripts/healthbar.gd
+++ GUI/Scripts/healthbar.gd
@@ -3,13 +3,13 @@ var maxhealth = 100
 var health
 
 func _on_interface_max_health_changed(value):
-	$TextureProgressBar.max_value = value
+	$CenterContainer/HBoxContainer/TextureProgressBar.max_value = value
 	maxhealth = value
-	$NinePatchRect/Label.text = "%s/%s" % [health,value]
+	$CenterContainer/HBoxContainer/TextureProgressBar.text = "%s/%s" % [health,value]
 
 
-func _on_interface_health_changed(value, displacement):
-	$TextureProgressBar.value = value
+func _on_interface_health_changed(value, _displacement):
+	$CenterContainer/HBoxContainer/TextureProgressBar.value = value
 	health = value
-	$NinePatchRect/Label.text = "%s/%s" % [value,maxhealth]
+	$CenterContainer/HBoxContainer/TextureProgressBar/Label.text = "%s/%s" % [value,maxhealth]
 
