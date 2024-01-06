18e671631212c621483278576672faf4c4d5daff
masterpoag <72989636+masterpoag@users.noreply.github.com>
2024-01-05 22:21:30 -0600

    Update player.gd

--- entities/scripts/player.gd
+++ entities/scripts/player.gd
@@ -1,13 +1,19 @@
 extends CharacterBody2D
 
+# UI system
 signal health_changed(value, displacement)
 signal exp_change(value)
 signal max_health_changed(value)
 signal levelup(level)
+#----------------------------------------------------------------
 
 # CHARACTER STATS
-var max_hp = 100
-var strength = 1
+var max_hp = 100; var health = 100; var maxhealth = 100
+var strength = 1; var damage = 10
+var speed = 75
+
+
+#----------------------------------------------------------------
 
 # LEVELING SYSTEM
 var level = 1
@@ -46,53 +52,66 @@ func level_up():
 	if health != maxhealth:
 		health = maxhealth
 		health_changed.emit(maxhealth,maxhealth-health)
+#----------------------------------------------------------------
 
+# Inventory system
+@export var inv: Inv
+#----------------------------------------------------------------
+
+#Char customization
 @onready var bodysprite = $sprite/Body
 @onready var clothessprite = $sprite/Clothes
 @onready var eyessprite = $sprite/Eyes
 @onready var hairsprite = $sprite/Hair
 @onready var accsprite = $sprite/Acc
-
 const sprites = preload("res://entities/art/art.gd")
-var speed = 75
+#----------------------------------------------------------------
+
 var current_dir
 
+
+#combat system
 var enemyRange = false
 var cooldown= 20
 var inCooldown = false
-var health = 100
-var maxhealth = 100
-var globalBody
-var playerAlive = true
-var damage = 10
 var attack = false
 var healingStart = false
 var damageable = true
 var invable = 0
+var playerAlive = true
+var globalBody
+#----------------------------------------------------------------
+
+
 #logging
 var last = 0
-
+#----------------------------------------------------------------
 
 func _ready():
+	attack = false
+	# this shouldnt be needed anymore
+#	if !Global.firstvisit && get_tree().get_current_scene().get_name() == "city":
+#		position.x= -38
+#		position.y= 47
 	
-	if !Global.firstvisit && get_tree().get_current_scene().get_name() == "city":
-		position.x= -38
-		position.y= 47
+#	sets char to what the player wanted
 	hairChange(Global.hair)
 	bodyChange(Global.body)
 	eyeChange(Global.eyes)
 	clothesChange(Global.clothes)
 	accChange(Global.acc)
+	#----------------------------------------------------------------
 	
+#	sets modulation that the player wants
 	hairsprite.self_modulate = Global.hair_mod
 	bodysprite.self_modulate = Global.body_mod
 	accsprite.self_modulate = Global.acc_mod
 	eyessprite.self_modulate = Global.eyes_mod
 	clothessprite.self_modulate = Global.clothes_mod
-	
-	
-	attack = false
+	#----------------------------------------------------------------
+	# make sure no animation is playing
 	$sprite/AnimationPlayer.pause()
+	#----------------------------------------------------------------
 
 func hairChange(factor):
 	hairsprite.texture = sprites.hair[factor]
@@ -114,29 +133,44 @@ func bodyChange(factor):
 	#bodysprite.Modulate(randi_range(0,255),randi_range(0,255),randi_range(0,255))
 
 func _process(_delta):
+#	checks if attacking
 	if playerAlive:
 		attackAction()
+#----------------------------------------------------------------
+	
+#	adds old mario style hit animation
 	if !$invframes.is_stopped():
 		invable+= 1
 		if invable % 2 == 0:
 			self.modulate = "ffffffff"
 		else:
 			self.modulate = "ffffff00"
+#----------------------------------------------------------------
+			
+#	gains EXP from global script
 	if Global.EXPToAdd > 0:
 		gain_experience(Global.EXPToAdd)
 		Global.EXPToAdd = 0
+#----------------------------------------------------------------
+
 
 func _physics_process(delta):
+#	attack timer
 	if $attackTimer.time_left <= 0:
 		attack = false
+#----------------------------------------------------------------
+
+#	player movement
 	if playerAlive:
 		playerMovement(delta)
-	
+#----------------------------------------------------------------
+
+#	death tracker
 	if health <=0:
 		health = 0
 		playerAlive = false
 		print("you Died")
-	
+#----------------------------------------------------------------
 
 func playerMovement(_delta):
 	if !attack:
@@ -173,16 +207,14 @@ func playerMovement(_delta):
 		Global.posy = position.y
 		if(Input.is_action_pressed("attack")):
 			play_anim(2)
-
 	healthUpdate()
 	
-func play_anim(m):
+func play_anim(m): #animations
 	var dir = current_dir
 	var anim = $sprite/AnimationPlayer
 	if  m == 0:
 		anim.stop()
 		anim.seek(0,true)
-	
 	if dir == "right":
 		if m == 1:
 			anim.play("right walk")
@@ -217,9 +249,7 @@ func attackAction():
 			anim.play("back attack")
 			$attackTimer.start()
 
-func player():
-	pass
-
+#checks for if a enemy hits player
 func enemy_attack(hit):
 	if damageable:
 		damageable = false
@@ -230,7 +260,7 @@ func enemy_attack(hit):
 		$invframes.start()
 
 
-
+#	stops players attack
 func _on_attack_timer_timeout():
 	$attackTimer.stop()
 	Global.playerAttacting = false
@@ -240,17 +270,19 @@ func healthUpdate():
 	if maxhealth != health:
 		if !healingStart:
 			healthregin()
-	
+
+# starts healing
 func _on_combat_timer_timeout():
 	if !healingStart:
 		$combatTimer.start()
 	healingStart = true
-	
+
+#	out of combat timer
 func healthregin():
 	$outOfCombatHeal.start()
 	
 	
-
+# healing func
 func _on_out_of_combat_heal_timeout():
 	if health != maxhealth:
 		health_changed.emit(health+5,5)
@@ -266,16 +298,17 @@ func _on_out_of_combat_heal_timeout():
 
 
 
-
-
+# checks for if you hit something
 func _on_weapon_body_entered(body):
-	
 	if body.is_in_group("Enemy"):
 		body.hit(damage)
 
-
+# stops mario style hit animation
 func _on_invframes_timeout():
 	$invframes.stop()
 	damageable = true
 	invable	= 0
 	self.modulate = "FFFFFFFF"
+
+
+#end of script
