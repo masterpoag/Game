3c756898d6eea172e64288277fa1797868a6996c
masterpoag <72989636+masterpoag@users.noreply.github.com>
2024-01-06 21:39:57 -0600

    NPC now moves but isnt random yet

--- entities/scripts/NPCBase.gd
+++ entities/scripts/NPCBase.gd
@@ -4,10 +4,11 @@ const speed = 30
 var currentState = 2
 var isRoaming = true
 var is_chatting = false
-
+var directions = [Vector2.RIGHT,Vector2.DOWN,Vector2.UP,Vector2.LEFT]
+var timer = [0.2, .5, 1, 1.5]
 var player
 var playerIsChatZone = false
-
+var states = [IDLE,new_DIR, MOVE]
 var direction = Vector2.RIGHT
 var startPos
 enum {
@@ -36,18 +37,29 @@ func _process(delta):
 			$sprite/AnimationPlayer.play("back walk")
 		if direction.y == 1:
 			$sprite/AnimationPlayer.play("front walk")
+	if isRoaming:
+		match currentState:
+			IDLE:
+				pass
+			new_DIR:
+				direction = directions[randi() % directions.size()]
+			MOVE:
+				move()
 
 
 func _ready():
 	randomize()
 	startPos = position
-	
-	
 	hairChange(randi_range(0,sprites.hair.size()-1))
 	bodyChange(randi_range(0,sprites.body_spritesheet.size()-1))
 	eyeChange(randi_range(0,sprites.eyes_spritesheet.size()-1))
 	clothesChange(randi_range(0,sprites.clothes_spritesheet.size()-1))
 	accChange(randi_range(0,sprites.acc_spritesheet.size()-1))
+
+func move():
+	if !is_chatting:
+		velocity += direction * (speed*.01)
+		move_and_slide()
 
 
 func hairChange(factor):
@@ -68,3 +80,19 @@ func eyeChange(factor):
 func bodyChange(factor):
 	bodysprite.texture = sprites.body_spritesheet[factor]
 	#bodysprite.Modulate(randi_range(0,255),randi_range(0,255),randi_range(0,255))
+
+
+func _on_chat_detection_area_body_entered(body):
+	if body.is_in_group("Player"):
+		player = body
+		playerIsChatZone = true
+
+
+func _on_chat_detection_area_body_exited(body):
+	if body.is_in_group("Player"):
+		playerIsChatZone= false
+
+
+func _on_timer_timeout():
+	$Timer.wait_time = timer[randi() % timer.size()]
+	currentState = states[randi() % states.size()]
